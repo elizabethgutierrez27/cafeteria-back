d6debc8b622a4e3f212576460eb28a72be97f707
elizabethgutierrez27 <146129308+elizabethgutierrez27@users.noreply.github.com>
2025-11-28 01:21:32 -0600

    ok

--- db.js
+++ db.js
@@ -5,9 +5,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 5432
+   host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'qwerty',
+  database: process.env.DB_NAME || 'cafe_db',
+  port: Number(process.env.DB_PORT) || 5432,
+  max: 10,
+  idleTimeoutMillis: 30000,
 });
